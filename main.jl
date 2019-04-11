4be1ba96cd6bed0404d4c970c597cb033dd3aa7b
gabihecksher <gabriela.hecksher@gmail.com>
2019-04-11 20:02:47 -0300

    boolean de igualdade funcionando

--- main.jl
+++ main.jl
@@ -13,19 +13,20 @@ using ParserCombinator
 #
 mutable struct Neg children end #onde neg = negacao do numero
 mutable struct Num children end
-mutable struct Inv children end #onde inv = inverso do numero 
+mutable struct Inv children end #onde inv = inverso do numero
 mutable struct Sum children end
 mutable struct Mul children end
+mutable struct Bool children end
 
 ##A GRAMATICA:
-positivo = Delayed() 
+positivo = Delayed()
 #Delayed() define um loop na gramatica
 
-valor = E"(" + positivo + E")" | PFloat64() 
+valor = E"(" + positivo + E")" | PFloat64()
 #O E determina expressao enquanto o + seria juntar as expressoes numa lista. E o | seria um ou.
 #ou seja o valor pode ser uma expressao (positiva) ou um numero ponto flutuante
 
-negativo = Delayed() 
+negativo = Delayed()
 negativo.matcher = valor | (E"-" + negativo > Neg)
 #aqui ele permite varias negacoes em sequencia tipo --3
 # o .matcher indica que a expressao "negativo" significa o que ta depois do =
@@ -42,7 +43,7 @@ div = E"/" + negativo > Inv
 #aqui eu to definindo que a divisao vai ser uma multiplicacao do numero inverso ou seja 3/2 => 3*(1/2)
 
 num = negativo[0:end] |> Num
-#aqui o [0:end] significa que vai ler os numeros em sequencia ate acabar os valores e entrar uma outra expressao ex: 123*2 => aqui ele vai ler "123" 
+#aqui o [0:end] significa que vai ler os numeros em sequencia ate acabar os valores e entrar uma outra expressao ex: 123*2 => aqui ele vai ler "123"
 
 multiplicacao.matcher = negativo + (mul | div)[0:end] |> Mul
 #o .matcher define que a expressao da multiplicacao sera  definida ao que vem depois do =
@@ -57,8 +58,18 @@ test_soma = positivo + Eos()
 test_mul = multiplicacao + Eos()
 #O Eos() faz a checagem se a entrada que eu dei faz sentido com o que eu defini na gramatica
 
-test_soma_mul = positivo + multiplicacao +Eos()
+test_soma_mul = positivo + multiplicacao + Eos()
+
+#TENTANDO FAZER O BOOLEANO FUNCIONAR
+bool_exp = Delayed()
+bool_exp.matcher = (E"Eq(" + positivo + E"," + positivo[0:end] + E")") |> Bool #  | "Not(" + positivo + ")"
+teste_bool = bool_exp + Eos()
 
 # println(parse_one("2/-2", test_div))
 # println(parse_one("1*2", test_mul))
-println(parse_one("1+2/3", test_soma_mul))
+
+#println(parse_one("1+2/3", test_soma_mul))
+#println(parse_one("4+1==3-9", teste_bool))
+println(parse_one("Eq(2+1,3+4)", teste_bool))
+#parse_dbg("1+2/3", Trace(test_soma_mul))
+
