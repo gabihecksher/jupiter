4a5288f82539a9eab7a468d6638f4a6fd41a453a
julianoce <julianoce@hotmail.com>
2019-04-16 21:01:15 -0300

    calculadora funcionando junto com bool e assign

--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
 using ParserCombinator
-
+using Nullables
+import Base.==
 # function pega_entrada(mensagem)
 #     print(mensagem)
 #     readline()
@@ -10,79 +11,92 @@ using ParserCombinator
 
 # tokens = split(entrada, "")
 # println(tokens)
-#
-mutable struct Neg children end #onde neg = negacao do numero
-mutable struct Num children end
-mutable struct Inv children end #onde inv = inverso do numero
-mutable struct Sum children end
-mutable struct Mul children end
-mutable struct Bool children end
-mutable struct Assign children end
-mutable struct Exp children end
 
-##A GRAMATICA:
-positivo = Delayed()
-#Delayed() define um loop na gramatica
+signed_prod(lst) = length(lst) == 1 ? lst[1] : Base.prod(lst)
+signed_sum(lst) = length(lst) == 1 ? lst[1] : Base.sum(lst)
 
-valor = (E"(" + positivo + E")") | PFloat64()
-#O E determina expressao enquanto o + seria juntar as expressoes numa lista. E o | seria um ou.
-#ou seja o valor pode ser uma expressao (positiva) ou um numero ponto flutuante
+abstract type Node end
+==(n1::Node, n2::Node) = isequal(n1.val, n2.val)
+calc(n::Float64) = n
+mutable struct Inv<:Node val end
+calc(i::Inv) = 1.0/calc(i.val)
+mutable struct Neg<:Node val end
+calc(n::Neg) = -calc(n.val)
+mutable struct Mul<:Node val end
+calc(p::Mul) = signed_prod(map(calc, p.val))
+mutable struct Sum<:Node val end
+calc(s::Sum) = signed_sum(map(calc, s.val))
+mutable struct Bool<:Node val end
+mutable struct Assign<:Node val end
 
-negativo = Delayed()
-negativo.matcher = valor | (E"-" + negativo > Neg)
-#aqui ele permite varias negacoes em sequencia tipo --3
-# o .matcher indica que a expressao "negativo" significa o que ta depois do =
-# o ">" serve para indicar que tudo que ta na esquerda vai virar uma funcao Neg
+@with_names begin
+    spc = Drop(Star(Space()))
+    #isso permite que eu bote espacos entre operacoes
 
-multiplicacao = Delayed()
+    @with_pre spc begin
 
-mul = E"*" + negativo
-#ou seja a multiplicacao pode ser de um valor positivo ou negativo(ja que o negativo chama um valor positivo) aqui percebemos que estamos montando
-#uma arvore igual ele deu em aula
+        ##A GRAMATICA:
+        positivo = Delayed()
+        #Delayed() define um loop na gramatica
 
+        valor = (E"(" + spc + positivo + spc + E")") | PFloat64()
+        #O E determina expressao enquanto o + seria juntar as expressoes numa lista. E o | seria um ou.
+        #ou seja o valor pode ser uma expressao (positiva) ou um numero ponto flutuante
 
-div = E"/" + negativo > Inv
-#aqui eu to definindo que a divisao vai ser uma multiplicacao do numero inverso ou seja 3/2 => 3*(1/2)
-
-num = negativo[0:end] |> Num
-#aqui o [0:end] significa que vai ler os numeros em sequencia ate acabar os valores e entrar uma outra expressao ex: 123*2 => aqui ele vai ler "123"
-
-multiplicacao.matcher = negativo + (mul | div)[0:end] |> Mul
-#o .matcher define que a expressao da multiplicacao sera  definida ao que vem depois do =
-
-add = E"+" + num
-sub = E"-" + num > Neg
-
-positivo.matcher = negativo + (add | sub)[0:end] |> Sum
+        negativo = Delayed()
+        negativo.matcher = Nullable{Matcher}(valor | (E"-" + negativo > Neg))
+        #aqui ele permite varias negacoes em sequencia tipo --3
+        # o .matcher indica que a expressao "negativo" significa o que ta depois do =
+        # o ">" serve para indicar que tudo que ta na esquerda vai virar uma funcao Neg
 
 
-test_soma = positivo + Eos()
-test_mul = multiplicacao + Eos()
-#O Eos() faz a checagem se a entrada que eu dei faz sentido com o que eu defini na gramatica
+        mul = E"*" + negativo
+        #ou seja a multiplicacao pode ser de um valor positivo ou negativo(ja que o negativo chama um valor positivo) aqui percebemos que estamos montando
+        #uma arvore igual ele deu em aula
 
-aritmetico = positivo + multiplicacao + Eos()
 
-#TENTANDO FAZER O BOOLEANO FUNCIONAR
-bool_exp = Delayed()
+        div = E"/" + negativo > Inv
+        #aqui eu to definindo que a divisao vai ser uma multiplicacao do numero inverso ou seja 3/2 => 3*(1/2)
 
-bool_exp.matcher = (E"Eq(" + (positivo|multiplicacao) + E"," + (positivo|multiplicacao)[0:end] + E")") | (E"Not(" + (positivo|multiplicacao)[0:end] + E")") |> Bool
+        prd = negativo + (mul | div)[0:end] |> Mul
+        #o .matcher define que a expressao da multiplicacao sera  definida ao que vem depois do =
 
-string = Word()
+        add = E"+" + prd
+        sub = E"-" + prd > Neg
 
-cmd_assign = Delayed()
 
-cmd_assign = (E"Assign(" + string + E"," + (positivo|multiplicacao|bool_exp)[0:end] + E")") |> Assign  
+        positivo.matcher = Nullable{Matcher}(prd + (add | sub)[0:end] |> Sum)
 
-teste_bool = bool_exp + Eos()
 
-teste_assign = cmd_assign + Eos()
+        #O Eos() faz a checagem se a entrada que eu dei faz sentido com o que eu defini na gramatica
 
-# println(parse_one("2/-2", test_div))
-# println(parse_one("1*2", test_mul))
+        aritmetico = spc + positivo  + Eos()
 
-# println(parse_one("1+", test_soma_mul))
-#println(parse_one("4+1==3-9", teste_bool))
+        # #TENTANDO FAZER O BOOLEANO FUNCIONAR
+        bool_exp = Delayed()
+
+        bool_exp.matcher = (E"Eq(" + (positivo) + E"," + (positivo)[0:end] + E")") | (E"Not(" + (positivo)[0:end] + E")") |> Bool
+
+        string = Word()
+
+        cmd_assign = Delayed()
+
+        cmd_assign = (E"Assign(" + string + E"," + (positivo|bool_exp)[0:end] + E")") |> Assign  
+
+        teste_bool = bool_exp + Eos()
+
+        # teste_assign = cmd_assign + Eos()
+    end
+end
+
+## TESTE CALCULADORA#
+println(parse_one("2-1 * 2/3", aritmetico))
+println(calc(parse_one("2-1 * 3/3", aritmetico)[1]))
+
+### TESTE BOOLEANO ####
 # println(parse_one("Eq(2-1,3/4)", teste_bool))
-println(parse_one("Assign(a,3/8)", cmd_assign))
 # println(parse_one("Eq((3*4)+1,13)", teste_bool))
-#parse_dbg("1+2/3", Trace(test_soma_mul))
+
+### TESTE ASSIGN ####
+# println(parse_one("Assign(a,3/8)", cmd_assign))
+
--- makefile
+++ makefile
@@ -2,9 +2,9 @@ install:
 	wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.0-linux-x86_64.tar.gz
 	tar zxf julia-1.0.0-linux-x86_64.tar.gz
 	rm julia-1.0.0-linux-x86_64.tar.gz
-	ln -s julia-1.0.0/bin/julia /usr/local/bin/julia
-	ln -s julia-1.0.0/bin/julia /usr/local/sbin/julia
-	echo $(PATH)
+	# ln -s julia-1.0.0/bin/julia /usr/local/bin/julia
+	# ln -s julia-1.0.0/bin/julia /usr/local/sbin/julia
+	# echo $(PATH)
 run:
 	cd julia-1.0.0/bin
 	./julia
