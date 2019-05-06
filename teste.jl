a5afa06e39285f52976a1968ad7e935d743ccf7c
julianoce <julianoce@hotmail.com>
2019-05-05 21:06:31 -0300

    varias expressoes funcionando

--- teste.jl
+++ teste.jl
@@ -1,5 +1,6 @@
 using ParserCombinator
 using Nullables
+import Base.==
 
 abstract type Node end
 ==(n1::Node, n2::Node) = isequal(n1.val, n2.val)
@@ -10,33 +11,71 @@ mutable struct Sum<:Node val end
 mutable struct Sub<:Node val end
 mutable struct Mul<:Node val end
 mutable struct Div<:Node val end
+mutable struct Id<:Node val end
+mutable struct Not<:Node val end
+mutable struct ArithExp<:Node val end
+mutable struct BoolExp<:Node val end
+mutable struct Exp<:Node val end
+mutable struct Eq<:Node val end
+
+
 
 @with_names begin
     spc = Drop(Star(Space()))
     # isso permite que eu bote ou nao espacos entre operacoes
+   
     @with_pre spc begin
 
-        Exp = Delayed()
 
-        digits = PFloat64() > Num
-        sum = (Exp+spc + E"+" + spc+ Exp) | digits |> Sum
+        ##################### ARIT EXPRESSION ########################################333
+        aexp = Delayed()
+        #Delayed() define um loop na gramatica
 
-        # sum = (Exp + E"+" + Exp) |> Sum
-        sub =  (Exp + spc + E"-" + spc + Exp) |> Sub
-        mul = (Exp + spc + E"*" + spc + Exp) |> Mul
-        div = (Exp + spc + E"/" + spc + Exp) |> Div
+        num = (E"(" + spc + aexp + spc + E")") | PFloat64() > Num
+        
+        multiplication = E"*" + num
 
-        # nos slides tem <exp> ao inves de <arithexp> mas nao sei se poderia por exemplo somar duas expressoes booleanas
-        ArithExp = (sum | sub | mul | div | digits) + Eos() # | sub | mul | div | digits
+        division = E"/" + num
 
-        Exp.matcher = Nullable{Matcher}(digits[0:end]) # | BoolExp
+        prd = (num + (multiplication)[0:end] |> Mul)
+
+        div = (num + (division)[0:end] |> Div)
+
+        add = E"+" + num 
+        sub = E"-" + num
+
+        soma = (num + (add)[0:end] |> Sum)
+        subtracao = (num + (sub)[0:end] |> Sub)
 
 
+        # aexp.matcher =(num + (add)[0:end]|> Sum)
+        aexp.matcher = Nullable{Matcher}((soma | subtracao | prd | div | num))
 
-        teste_digits = ArithExp
-        # teste_sum = Exp + Eos()
 
-        teste =  teste_digits
+        aritmetico = spc + aexp > ArithExp
+
+        # teste = aritmetico
+
+        ############################ BOOL EXP ##################################
+        
+        bool_exp = Delayed()
+
+        expression = (bool_exp | aritmetico)[0:end] |> Exp
+
+        negation = E"not" + spc + (aritmetico)[0:end]|> Not
+
+        equality = spc + (aritmetico) + spc + E"==" + spc + (aritmetico)[0:end] |> Eq
+
+        bool_exp.matcher =  Nullable{Matcher}((equality | negation))
+
+        # id = Word()[0:end] |> Id 
+
+        # truth = E"True" | E"False"
+
+
+        booleano = bool_exp + Eos() > BoolExp
+        teste = booleano
+
 
     end
 end
@@ -51,3 +90,4 @@ println("Você digitou $entrada")
 
 parse = parse_one(entrada, teste)
 println(parse)
+
