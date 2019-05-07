580fad1d6581de014b51532b3266e9ab4bc59472
julianoce <julianoce@hotmail.com>
2019-05-06 22:21:03 -0300

    commit pra passar a raiva que eu tive de voltar do trabalho morta e fazer esse trabalho

--- teste.jl
+++ teste.jl
@@ -1,6 +1,5 @@
 using ParserCombinator
 using Nullables
-import Base.==
 
 abstract type Node end
 ==(n1::Node, n2::Node) = isequal(n1.val, n2.val)
@@ -13,12 +12,14 @@ mutable struct Mul<:Node val end
 mutable struct Div<:Node val end
 mutable struct Id<:Node val end
 mutable struct Not<:Node val end
-mutable struct ArithExp<:Node val end
-mutable struct BoolExp<:Node val end
 mutable struct Exp<:Node val end
 mutable struct Eq<:Node val end
 mutable struct Boo<:Node val end
-
+mutable struct Lt<:Node val end
+mutable struct Le<:Node val end
+mutable struct Gt<:Node val end
+mutable struct Ge<:Node val end
+mutable struct And<:Node val end
 
 
 
@@ -33,7 +34,8 @@ mutable struct Boo<:Node val end
         aexp = Delayed()
         #Delayed() define um loop na gramatica
 
-        num = (E"(" + spc + aexp + spc + E")") | PFloat64() > Num
+        # num = (E"(" + spc + aexp + spc + E")") | PFloat64() > Num
+        num = (E"(" + spc + PFloat64() + spc + E")") | PFloat64() > Num
         
         multiplication = E"*" + num
 
@@ -51,10 +53,9 @@ mutable struct Boo<:Node val end
 
 
         # aexp.matcher =(num + (add)[0:end]|> Sum)
-        aexp.matcher = Nullable{Matcher}((soma | subtracao | prd | div | num))
+        aexp.matcher = (num | soma | subtracao | prd | div)
 
-
-        aritmetico = spc + aexp > ArithExp
+        aritmetico = spc + aexp
 
         # teste = aritmetico
 
@@ -62,23 +63,31 @@ mutable struct Boo<:Node val end
         
         bool_exp = Delayed()
 
-        expression = (bool_exp | aritmetico)[0:end] |> Exp
+        expression = Delayed()
+
 
         negation = E"not" + spc + (aritmetico)[0:end]|> Not
 
         equality = spc + (aritmetico) + spc + E"==" + spc + (aritmetico)[0:end] |> Eq
+        lower_than = spc + (aritmetico) + spc + E"<" + spc + (aritmetico)[0:end] |> Lt
+        lower_equal = spc + (aritmetico) + spc + E"<=" + spc + (aritmetico)[0:end] |> Le
+        greater_than = spc + (aritmetico) + spc + E">" + spc + (aritmetico)[0:end] |> Gt
+        greater_equal = spc + (aritmetico) + spc + E">=" + spc + (aritmetico)[0:end] |> Ge
 
-        truth = E"True" | E"False" |>Boo
 
 
-        bool_exp.matcher =  Nullable{Matcher}((equality | negation | truth))
+        truth = E"True" | E"False" |> Boo
+
+
+        bool_exp.matcher =  Nullable{Matcher}((equality | negation | truth|lower_equal|lower_than|greater_equal|greater_than))
 
         # id = Word()[0:end] |> Id 
 
+        booleano = bool_exp
 
+        expression.matcher = (booleano | aritmetico)[0:end] |> Exp
 
-        booleano = bool_exp + Eos() > BoolExp
-        teste = booleano
+        teste = expression + Eos()
 
 
     end
