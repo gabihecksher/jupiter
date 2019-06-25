29d1244000ff87553dbcd3b3172a3bd227fbbcbc
julianoce <julianoce@hotmail.com>
2019-06-24 23:44:28 -0300

    tirei a main2 e o arquivo any agora eh so main

--- src/main.jl
+++ src/main.jl
@@ -2,69 +2,34 @@ include("./automaton.jl")
 include("./lexer.jl")
 
 
-function pega_entrada()
-     println("Escreva a expressão: ")
-     readline()
- end
-
- function pega_arquivo()
-    println("Escreva o o path do arquivo a ser lido. Exemplo: test/fatorial.txt")
-    readline()
- end
-
-
-function main()
-    println("Pressione 1 para escrever o programa. Exemplos: 2+3 , 2<3 and 3>11 ")
-    println("Pressione 2 para ler um arquivo texto como entrada")
-    digito = readline()
-    env = Dict()
-    store = Dict()
-
-    println("De quantas variaveis seu programa vai precisar?")
-    num_variaveis = parse(UInt8, readline())
-    for i = 1:num_variaveis
-        println("Qual o nome da $i ª variavel?")
-        nome_var = readline()
-        println("Qual o valor inicial da $i ª variavel?")
-        valor_var = readline()
-
-        env[nome_var] = i # coloca o id da variavel no dicionario env associada a localizacao
-        store[i] = valor_var # associa a localizacao da variavel ao seu valor no dicionario store
-        print_variables(env,store)
-    end
-
-    if (isequal(digito, "1"))
-
-        entrada = pega_entrada()
-        println("Entrada: $entrada")
-
-        parser = parse_one(entrada, teste)
-        println(parser)
-
-
-        # Automaton.automaton([parser_string], [], env, store)
-        control_stack = Array{Any, 1}(parser)
-        value_stack = Array{Any, 1}()
-        automaton(control_stack, value_stack, env, store)
-
-    elseif (isequal(digito, "2"))
-        entrada = pega_arquivo()
-        open(entrada) do file
+function main(args)
+    if(isequal(args[1], "-f"))
+        arquivo_path = args[2]
+        open(arquivo_path) do file
             for ln in eachline(file)
                 linha = "$(ln)"
                 println(linha)
                 parse = parse_one(linha, teste)
-                parser_string = (string.(parse))
-                parser_string = replace(parser_string[1], "Any"=> "")
-                parser_string = replace(parser_string, "["=> "")
-                parser_string = replace(parser_string, "]"=> "")
-                parser_string = replace(parser_string, " "=> "")
-                println(parser_string)
-                Automaton.automaton([parser_string], [], env, store)
-                println(" ")
+                control_stack = Array{Any, 1}(parse)
+                value_stack = Array{Any, 1}()
+                env = Dict{String,Int64}
+                store = Dict{Int64,PFloat64}
+                automaton(control_stack, value_stack, env, store)
             end
-        end
+    end
+
+    elseif(isequal(args[1], "-i"))
+        println("Escreva a expressão: ")
+        entrada = readline()
+        parser = parse_one(entrada, teste)
+        println(parser)
+         control_stack = Array{Any, 1}(parser)
+         value_stack = Array{Any, 1}()
+         env = Dict()
+         store = Dict()
+         automaton(control_stack, value_stack, env, store)
     end
 end
 
-main()
+
+main(ARGS)
